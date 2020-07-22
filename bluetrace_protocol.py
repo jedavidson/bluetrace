d3f1499c8107f27b3d5512c4614ce7bcf008c2e7
James Davidson <jedavidson2000@gmail.com>
2020-07-23 00:20:33 +1000

    Add protocol file for specifying protocol messages

---
+++ bluetrace_protocol.py
@@ -0,0 +1,19 @@
+# bluetrace_protocol.py: A specification of constants used in BlueTrace
+# by James Davidson for COMP3331, 20T2
+
+# Protocol messages sent by the server when authenticating the client.
+INITIATING_AUTH = 'BT_AUTH_INIT'.encode()
+READY_TO_AUTH = 'BT_AUTH_READY'.encode()
+EXPECTING_USERNAME = 'BT_AUTH_UN'.encode()
+EXPECTING_PASSWORD = 'BT_AUTH_PW'.encode()
+
+# Informative messages to relay to the client during authentication.
+AUTHENTICATION_SUCCESS = 'Welcome to the BlueTrace simulator!'.encode()
+INVALID_CREDENTIALS = 'Invalid password. Please try again.'.encode()
+ACCOUNT_NOW_BLOCKED = 'Invalid password. Your account has been blocked. ' \
+                      'Please try again later.'.encode()
+ACCOUNT_IS_BLOCKED = 'Your account is blocked due to multiple login failures. ' \
+                     'Please try again later.'.encode()
+
+# The protocol message sent by the client when logging out.
+CLIENT_LOGGING_OUT = 'BT_AUTH_LOGOUT'.encode()
