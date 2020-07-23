95c3c85ee931942be24ee867ac08af4507fed7cd
James Davidson <jedavidson2000@gmail.com>
2020-07-24 00:48:19 +1000

    Implement means for client to download temp IDs on the server side

--- server.py
+++ server.py
@@ -6,7 +6,10 @@
 import sys
 import threading
 import time
+from datetime import datetime, timedelta
+from random import choice
 from socket import *
+from string import digits
 
 import bluetrace_protocol
 
@@ -21,6 +24,7 @@ blocked_users = {}
 # Various thread locks to regulate concurrent resource access.
 blocked_users_lock = threading.Lock()
 credentials_lock = threading.Lock()
+temp_ids_lock = threading.Lock()
 
 ''' Helper functionality '''
 
@@ -95,13 +99,28 @@ def block(client, username):
     client.send(bluetrace_protocol.ACCOUNT_NOW_BLOCKED)
 
 
+def generate_timestamp(dt, offset=0):
+    '''
+    Returns a timestamp in the format DD/MM/YY HH:MM:SS for the given datetime
+    with a certain offset (in minutes).
+
+    If no offset is given, the offset is taken to be 0.
+    '''
+
+    return (dt + timedelta(minutes=offset)).strftime('%d/%m/%y %H:%M:%S')
+
+
 ''' Main functionality '''
 
 
 def authenticate(client):
     '''
     Authenticates an incoming connection.
-    The result of the authentication process is returned.
+
+    The result of the authentication process is returned along with the
+    client's username.
+    
+    If authentication fails, the username returned is None.
     '''
 
     # Initiate authentication with the connecting client.
@@ -117,27 +136,63 @@ def authenticate(client):
     # If the client is blocked, tell them and end authentication.
     if is_blocked(username):
         client.send(bluetrace_protocol.ACCOUNT_IS_BLOCKED)
-        return False
+        return False, None
 
     # Verify the given password, and block them if they take too many attempts.
     attempts = verify_password(client, username, password)
     if attempts == 3:
         block(client, username)
-        return False
+        return False, None
 
     # Otherwise, send a success message and end authentication.
     client.send(bluetrace_protocol.AUTHENTICATION_SUCCESS)
-    return True
+    return True, username
+
+
+def generate_temp_id(username):
+    '''
+    Returns a new temp ID for a user, valid for 15 minutes.
+    
+    An appropriate entry in the temp IDs file is written, and if this file
+    does not already exist beforehand, it will be created.
+    '''
+
+    temp_id = ''.join(choice(digits) for _ in range(20))
+
+    with temp_ids_lock:
+        with open('tempIDs.txt', 'a+') as temp_ids:
+            start = generate_timestamp(datetime.now())
+            end = generate_timestamp(datetime.now(), offset=15)
+            temp_ids.write(f'{username} {temp_id} {start} {end}')
+
+    print(f'Temp ID {temp_id} generated for {username}.')
+    return temp_id
+
+
+def handle_request(client, username, request):
+    ''' Handles a request issued by the client. '''
+
+    if request == bluetrace_protocol.DOWNLOAD_TEMP_ID:
+        temp_id = generate_temp_id(username)
+        client.send(temp_id.encode())
+    elif request == bluetrace_protocol.UPLOAD_CONTACT_LOG:
+        # TODO: Handle uploading contact logs.
+        pass
 
 
 def handle_connection(client):
     ''' Handles an incoming connection. '''
 
     # Authenticate the incoming connection first.
-    if not authenticate(client):
+    authenticated, username = authenticate(client)
+    if not authenticated:
         return
 
-    # TODO: Handle the rest of the connection.
+    # Receive requests from the client until they initiate the logout phase.
+    request = client.recv(1024)
+    while request != bluetrace_protocol.LOGOUT_CLIENT:
+        handle_request(client, username, request)
+        request = client.recv(1024)
 
 
 def server(port):
